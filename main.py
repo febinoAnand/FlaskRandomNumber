1e5815f2d30205291531c3f0de9dc99126bab85a
anandkumar <febinosolutions@gmail.com>
2024-02-26 10:44:26 +0530

    Added 0 to 255 post and get routes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask
+from flask import Flask,request
 import random
 app = Flask('__main__')
-
 
 @app.route('/')
 def index():
@@ -9,6 +8,26 @@ def index():
     output = f"{r}"
     return output
 
+@app.route('/iopin',methods=["POST","GET"])
+def ioPin():
+    if request.method == "POST":
+        r = request.form["inputs"]
+        output = int(r)
+        if output < 0 or output > 255:
+            r = "input data error"
+        else:
+            print (r)
+            randomNo = random.randint(0,255)
+            r = f"{randomNo}"
+    else:
+        r = "GET error"
+    return r
+
+@app.route('/output')
+def output():
+    r = random.randint(0,255)
+    output = f"{r}"
+    return output
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0")
